08358767dd8271541e115bff7154791b23546454
https://github.com/cjhorton19/PHPChapter11.git <30810541+cjhorton19@users.noreply.github.com>
2019-03-24 15:10:57 -0700

    Auth service changes

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,15 +1,57 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthService {
 
-    BASE_URL = 'http://localhost:63145/auth';
+    BASE_URL = 'http://localhost:58630/auth';
+    NAME_KEY = 'name';
+    TOKEN_KEY = 'token';
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private router: Router) { }
+
+    get name() {
+        return localStorage.getItem(this.NAME_KEY);
+    }
+
+    get isAuthenticated() {
+        return !!localStorage.getItem(this.TOKEN_KEY);
+    }
+
+    get tokenHeader() {
+        // tslint:disable-next-line:prefer-const
+        let header = new HttpHeaders({Authorization: 'Bearer ' + localStorage.getItem(this.TOKEN_KEY)});
+        return new Request({ headers: header});
+    }
+
+    login(loginData) {
+        this.http.post(this.BASE_URL + '/login', loginData).subscribe(res => {
+            this.authenticate(res);
+        })
+    }
 
     register(user) {
-        this.http.post(this.BASE_URL + '/register', user).subscribe();
+        delete user.confirmPassword;
+        this.http.post(this.BASE_URL + '/register', user).subscribe(res => {
+            this.authenticate(res);
+        });
+    }
+
+    logout() {
+        localStorage.removeItem(this.NAME_KEY);
+        localStorage.removeItem(this.TOKEN_KEY);
+    }
+
+    authenticate(res) {
+        var authResponse = res.json();
+
+        if (!authResponse.token)
+            return;
+
+        localStorage.setItem(this.TOKEN_KEY, authResponse.token);
+        localStorage.setItem(this.NAME_KEY, authResponse.firstName);
+        this.router.navigate(['/']);
     }
 
 }
